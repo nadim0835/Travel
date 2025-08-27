1e0a6c41525c9c9c54ac4176480624713d476462
nadim0835 <nadim0835@gmail.com>
2025-08-27 21:38:40 +0600

    some layout change

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
               <p className=" lg:text-[16px] text-[20px] mt-3 lg:mt-[30px] mb-3 lg:mb-0 leading-[30px] font-medium text-[#5E6282] font-Poppins  ">Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
               <div className=" flex items-center mb-4 mt-[34px]  gap-[6%] lg:gap-[44px] ">
                 <button className=" text-[18px] font-medium py-[19px] px-[27px] bg-[#F1A501] hover:bg-white
-                 hover:text-[#F1A501] duration-300 hover:border-[#F1A501] border text-white rounded-[10px]  ">Find out more</button>
+                 hover:text-[#F1A501] duration-300 hover:border-[#F1A501] border text-white rounded-[10px]  ">Find out</button>
                
                     <FaRegCirclePlay className=" bg-[#DF6951] text-white hover:h-[52px] hover:w-[52px] h-[40px] w-[40px] cursor-pointer ring-blue-300 duration-300 rounded-[50%]   " />
                     <p className=" text-[#686D77] font-medium font-Poppins text-[17px]  ">Play Demo</p>
